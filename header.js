1e4211568f3c2a6b724c43970561037ca654920a
Bovacu <vazquezborja@hotmail.es>
2023-02-23 17:45:14 -0800

    Set new logo!

--- header.js
+++ header.js
@@ -1,6 +1,6 @@
 document.write('<nav class="navbar navbar-expand-lg navbar-dark bg-dark">')
 document.write('<div class="container px-5">')
-document.write('<a class="navbar-brand" href="index.html"><img src="assets/logo.png" width="48" height="48" alt="..." /></a>')
+document.write('<a class="navbar-brand" href="index.html"><img src="assets/logo256.png" width="64" height="64" alt="..." /></a>')
 document.write('<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>')
 document.write('<div class="collapse navbar-collapse" id="navbarSupportedContent">')
 document.write('<ul class="navbar-nav ms-auto mb-2 mb-lg-0">')
